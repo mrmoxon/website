9abf672a104fb905e2096eb883462a7cd788f159
Oscar Moxon <oscarjmoxon@gmail.com>
2024-02-25 22:11:14 +0000

    primary edits

---
+++ oscarmoxon.com/src/lib/config/biotext.ts
@@ -0,0 +1,102 @@
+export const bioText = `
+<br>
+Some things I believe in:
+<br>
+<br>
+Path of civilisation:
+<ul>
+  <li>We have Paleolithic emotions, medieval institutions and godlike technology. - Edward O Wilson</li>
+  <li>The alignment problem defines the core dynamic/growth/fitness/sustainability of any civilisation. (swap order)</li>
+  <li>It is best summed up by Edward O. Wilson: "We have Paleolithic emotions, medieval institutions and godlike technology.”
+    <ul>
+      <li>The trajectory of civilisation is fundamentally shaped by our accumulating knowledge, which serves as a double-edged sword—empowering us to achieve the miraculous while exposing us to escalating existential risks.</li>
+      <li>Striking a balance between stability and creative disruption is the role of regulation.</li>
+    </ul>
+  </li>
+  <li>It is our moral duty to align ourselves with technology(?) and reshape the universe to our preferences, harnessing the laws of physics to guide entropy(?). Alignment is a multi-agent issue that will always evolve with society and technology. We should model the universe as a canvas, bounded at the edge only by the laws of physics.</li>
+  <li>Unchecked technological power converges on pure AGI, which humans in 2024 cannot handle.</li>
+  <li>Transformers offer a stable and scalable paradigm for artificially capable intelligence by '30 (v1 "AGI").</li>
+</ul>
+<br>
+
+Lessions from economics:
+<ul>
+  <li>The Efficient market hypothesis (EMH) is weak.</li>
+  <li>The richest opportunities emerge in the gaps of market rationality.</li>
+  <li>The fate of billions is surprisingly steered by the decisions of a few. In many cases it's more accurate to model the world as 500 people than 8 billion - Nat Friedman. Instead, we should empower and extend the information processing capabilities of all humans, not a top percentile.</li>
+  <li>Concentrations of power are typically dangerous, but always the best way/heuristic to maximise returns.</li>
+  <li>Digital platforms proliferate hyperstimuli, both actively and by nature. This converges at wireheading.</li>
+</ul>
+<br>
+
+Some other thoughts:
+<ul>
+  <li>Progress hinges on our willingness to question deeply held beliefs and the courage to ask more meaningful questions. There is no fix-all.</li>
+  <li>Better to derive satisfaction from the continual refinement and expansion of your ideas.</li>
+  <li>An audacity to tackle grand challenges best stems from a genuine excitement for the work itself.</li>
+  <li>Innovation and agility thrive in small, focused groups. The bloat of bureaucracy stifles creativity and efficiency: every organisation could achieve more with less. Innovators Dilemma II.</li>
+  <li>Education must transcend traditional boundaries.</li>
+</ul>
+<br>
+
+<ul>
+  <li>Victimless crimes wouldn't exist in a more efficient government with the individual autonomy as arbiter.</li>
+  <li>Three universal ways to measure civilisational progress and scalability: Kardashev Scale, Alignment Trifecta/Coefficient/Factor (cohesion between trifecta of individuals, institutions, and technology), and Assembly Theory/Complexity Coefficient (architecture).
+    <ul>
+      <li>Alignment Factor is a measure of cohesion with how society's biology, institutions, and technology are aligned. This is connected to their cohesiveness and synergy with their environment, the rules and freedoms we govern by, and the tools we develop to influence our environment.</li>
+      <li>Progress is not just about advancing in one area, but about ensuring that these advances improve our lives and are in balance with human needs to societal values. Also, if your technological progress is tangential to the nature of your other two components, you are doomed for derailment.</li>
+      <li>Assembly Theory measures the complexity of the civilisational system; a measure of the nature of the civilisation (we could have Kardashev Type 0 but superhigh complexity).</li>
+    </ul>
+  </li>
+</ul>
+<br>
+`
+
+// • The alignment problem outlines the central challenge/core/kernal operation/path of any civilisation.
+// <br>
+//   • It is best summed up by Edward O. Wilson: “We have Paleolithic emotions, medieval institutions and godlike technology.”
+//   ...
+
+
+// // src/lib/bioText.ts
+// export const bioText = 
+// `
+// Civilisation:
+
+//   • The alignment problem outlines the central challenge/core/kernal operation/path of any civilisation.
+//   • It is best summed up by Edward O. Wilson: “We have Paleolithic emotions, medieval institutions and godlike technology.” 
+//     ○ / The trajectory of civilisation is fundamentally shaped by our accumulating knowledge, which serves as a double-edged sword—empowering us to achieve the miraculous while exposing us to escalating existential risks. / Technology empowers us to achieve the miraculous while exposing us to escalating existential risks. 
+//     ○ Striking a balance between stability and creative disruption is the role of regulation. 
+  
+//   • It is our moral duty to align ourselves with technology(?) and reshape the universe to our preferences, harnessing the laws of physics to guide entropy(?). / Alignment is a multi-agent issue that will always evolve with society and technology. /  / we should model the universe as a canvas, bounded at the edge only by the laws of physics.
+  
+//   • Unchecked technological power leads to purer AGI, which humans cannot handle if we don't upgrade. Something has to budge.
+//   • Transformers offer a stable and scalable paradigm for artificially capable intelligence (v1 AGI).
+
+// Lessons from economics:
+
+//   • The Efficient market hypothesis (EMH) is weak. 
+//   • The richest opportunities emerge in the gaps of market rationality.
+//   • The fate of billions is surprisingly steered by the decisions of a few / In many cases it's more accurate to model the world as 500 people than 8 billion - Nat Friedman. / Instead, we should empower and extend the information processing capabilities of all humans, not a top percentile. 
+//   • Concentrations of power are typically dangerous, but /always the best way/heuristic to maximise returns.
+//   • Digital platforms proliferate hyperstimuli, both actively and by nature. This converges at wireheading. 
+
+
+
+//   • Victimless crimes wouldn't exist in a more efficient government with the individual autonomy as arbiter.
+
+//   • Three universal ways to measure civilisational progress and scalability: Kardashev Scale, Alignment Trifecta/Coefficient/Factor (cohesion between trifecta of individuals, institutions, and technology), and Assembly Theory/Complexity Coefficient (architecture). 
+//     ○ Alignment Factor is measure of cohesion with how society's biology, institutions, and technology are aligned. This is connected to their cohesiveness and synergy with their environment, the rules and freedoms we govern by, and the tools we develop to influence our environment. 
+//       § AKA Progress is not just about advancing in one area, but about ensuring that these advances improve our lives and are in balance with human needs to societal values. 
+//       § Also, if your technological progress is tangental to the nature of your other two components, you are doomed for derailment. 
+//     ○ Assembly Theory measures the complexity of the civilisational system; a measure of the nature of the civilisation (we could have Kardashev Type 0 but superhigh complexity). 
+
+// Philosophy:
+
+//   • Progress hinges on our willingness to question deeply held beliefs and the courage to ask more meaningful questions. There is no fix-all. 
+//   • Better to derive satisfaction from the continual refinement and expansion of your ideas.
+//   • An audacity to tackle grand challenges best stems from a genuine excitement for the work itself.
+//   • (/subsequently) Innovation and agility thrive in small, focused groups. The bloat of bureaucracy stifles creativity and efficiency: every organisation could achieve more with less. / Innovators Dilemma II. 
+//   • Education must transcend traditional boundaries
+
+// `;
---
+++ oscarmoxon.com/src/lib/config/site.ts
@@ -0,0 +1,19 @@
+import type { SiteConfig } from '$lib/types/site';
+import { bioText } from '$lib/config/biotext';
+
+export const site: SiteConfig = {
+  protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
+  title: 'MOXON',
+  subtitle: 'Oscar Moxon - Essays - Projects - About',
+  lang: 'en-US',
+  description: 'London-based artificial intelligence postgraduate, researching agents and multi-agent systems.',
+  author: {
+    // avatar: '/assets/maskable@192.png',
+    name: '',
+    // status: '🌸',
+    
+    bio: '',
+  },
+  themeColor: '#3D4451'
+}
